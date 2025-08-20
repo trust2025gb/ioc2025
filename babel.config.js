cde4e826ddea9be9448d93ba81ade1683673ed72
77882736 <77882736@163.com>
2025-08-20 15:13:51 +0800

    build(babel): add plugins for private methods/properties to satisfy @tanstack/query-core

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,9 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
       'react-native-reanimated/plugin'
     ],
   };
